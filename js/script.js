0ced204708f5e5d24e1f44906f5da9fcbb346731
ericaostini <ericaostini14@gmail.com>
2023-10-09 15:46:31 +0200

    display km and age

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
 let km = prompt("Quanti km vuoi percorrere?");
-console.log(km);
 
 let eta = prompt("Età: ");
-console.log(eta);
+
+let biglietto = `
+    <div>
+        Voglio percorrere ${km} e ho ${eta} anni
+    </div>
+`;
+
+document.getElementById("info-biglietto").innerHTML = biglietto;
 
 // verificare che i valori inseriti siano una variabile numerica e non di testo 
 if (isNaN(km) || isNaN(eta)){
