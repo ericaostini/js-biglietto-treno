107968b80b8d7104076e58818452af460299dca3
ericaostini <ericaostini14@gmail.com>
2023-10-09 15:52:40 +0200

    calculation of trip price

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
 let km = prompt("Quanti km vuoi percorrere?");
-
 let eta = prompt("Età: ");
 
 let biglietto = `
@@ -16,3 +15,13 @@ if (isNaN(km) || isNaN(eta)){
     console.log("false");
     window.location.reload();
 }
+
+// calcolo prezzo del viaggio 
+// prezzo fisso per km 0.21 euro al km
+// sconto del 20% per i minorenni 
+// sconto del 40% per over 65
+
+const pKM = 0.21;
+let costoViaggio = pKM * km;
+console.log(costoViaggio);
+
