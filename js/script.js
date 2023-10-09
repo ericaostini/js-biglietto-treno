ffdb4e611682f2529fb80ea7447229b51c517f30
ericaostini <ericaostini14@gmail.com>
2023-10-09 15:15:58 +0200

    prompt km to be covered and age of passenger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("ciao")
+// chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+let km = prompt("Quanti km vuoi percorrere?");
+console.log(km);
+
+let eta = prompt("Età: ");
+console.log(eta);
